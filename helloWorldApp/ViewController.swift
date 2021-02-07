e5cd3acf52e789562f30150dd8c1fd46fe832731
Te <arsevrukov@gmail.com>
2021-02-07 20:58:52 +0300

    Logic work

--- helloWorldApp/ViewController.swift
+++ helloWorldApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var showTextBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        showTextBtn.layer.cornerRadius = 20
     }
 
 
+    @IBAction func tapMeBtnPressed() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden.toggle()
+            showTextBtn.setTitle("Tap again!", for: .normal)
+        } else {
+            helloWorldLabel.isHidden.toggle()
+            showTextBtn.setTitle("Tap me!", for: .normal)
+        }
+    }
 }
 
